cfe05484ee584b3cb447b4cc22923b1652661bca
Mahtab Alam <smmalam9@gmail.com>
2021-10-20 22:30:33 +0530

    Bianry Tree print

---
+++ Binary Trees/PrintBinTree.java
@@ -0,0 +1,54 @@
+package binarytree;
+
+public class PrintBinTree {
+	public static void printBinTree(BinaryTreeNode<Integer> root) {
+		if (root == null) {
+			return;
+		}
+		System.out.print(root.data + ":");
+		if (root.left != null) {
+			System.out.print("L" + root.left.data + ":");
+		}
+		if (root.right != null) {
+			System.out.print("R" + root.right.data + ":");
+		}
+		System.out.println();
+		printBinTree(root.left);
+		printBinTree(root.right);
+
+	}
+
+	public static void printBinTree2(BinaryTreeNode<Integer> root) {
+		if (root == null) {
+			return;
+		}
+		System.out.print(root.data + "");
+
+		printBinTree(root.left);
+		printBinTree(root.right);
+
+	}
+
+	//
+
+	public static void main(String[] args) {
+		BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(1);
+		root.left = new BinaryTreeNode<Integer>(2);
+		root.right = new BinaryTreeNode<Integer>(3);
+		root.left.left = new BinaryTreeNode<Integer>(4);
+		root.left.right = new BinaryTreeNode<Integer>(5);
+		root.right.left = new BinaryTreeNode<Integer>(6);
+		root.right.right = new BinaryTreeNode<Integer>(7);
+		root.left.left.left = new BinaryTreeNode<Integer>(8);
+		root.left.left.right = new BinaryTreeNode<Integer>(9);
+		root.left.right.left = new BinaryTreeNode<Integer>(10);
+		root.left.right.right = new BinaryTreeNode<Integer>(11);
+		root.right.left.left = new BinaryTreeNode<Integer>(12);
+		root.right.left.right = new BinaryTreeNode<Integer>(13);
+		root.right.right.left = new BinaryTreeNode<Integer>(14);
+		root.right.right.right = new BinaryTreeNode<Integer>(15);
+		printBinTree(root);
+
+	}
+
+}
