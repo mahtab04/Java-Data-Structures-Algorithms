089430c8cd2a1a876422efea2d89eaa5ede92eab
Mahtab Alam <smmalam9@gmail.com>
2022-05-18 19:15:34 +0530

    cylic quadiliteral

---
+++ Codechef/codechef/CyclicQuadrilateral.java
@@ -0,0 +1,24 @@
+//https://www.codechef.com/problems/CYCLICQD
+package codechef;
+
+import java.util.Scanner;
+
+public class CyclicQuadrilateral {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            int d = sc.nextInt();
+
+            if (a + c == 180 && b + d == 180) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+        sc.close();
+    }
+}
